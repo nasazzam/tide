16233f59d4f3799e34cbf947c265130a62f58ea8
nasazzam <92422850+nasazzam@users.noreply.github.com>
2026-09-10 14:06:25 +0900

    Update package checksums after attribution cleanup

--- Formula/tide.rb
+++ Formula/tide.rb
@@ -7,10 +7,10 @@ class Tide < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/nasazzam/tide/releases/download/v0.2.0/tide-v0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "2e0edb55d7fba3aa20c71bde0a048d3f81ab8fedc1525451fc6d6ecfe50cdbd1"
+      sha256 "50301cf9730b8b815620451bcbbb90f523b06d7ac300eec992deaa5b45fa05e8"
     else
       url "https://github.com/nasazzam/tide/releases/download/v0.2.0/tide-v0.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 "ec731d4cab64ae48b2a005d1346996bccd84758f06c1c676df19b497f385258c"
+      sha256 "19c06cef17e0530e365e8a123d0551a74d40b717949af5ee441a98def46e8403"
     end
   end
 
