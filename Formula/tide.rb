a102aa13502bb65b24770aea4651f3abfc579215
nasazzam <92422850+nasazzam@users.noreply.github.com>
2026-09-10 18:20:49 +0900

    Update package metadata for v0.4.0

--- Formula/tide.rb
+++ Formula/tide.rb
@@ -7,10 +7,10 @@ class Tide < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/nasazzam/tide/releases/download/v0.4.0/tide-v0.4.0-aarch64-apple-darwin.tar.gz"
-      sha256 "b78e157b41ecf174647457b823c591b67cfd3e89ed9cdba49651656bddb4dacf"
+      sha256 "8b8181cf9ec4ef485fb0997e2cc8d61bd39e860bae99df7cbf74e08356035e7f"
     else
       url "https://github.com/nasazzam/tide/releases/download/v0.4.0/tide-v0.4.0-x86_64-apple-darwin.tar.gz"
-      sha256 "53963467d9b6752e6f35e024e370eb33c39628681d5c8bb9b296b6ad85230b8c"
+      sha256 "37578aa619c957d039cee6f70f132b2d70fedcaa658fd53f69fadf4ccce2af9b"
     end
   end
 
